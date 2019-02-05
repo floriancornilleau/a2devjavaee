1b3e88ee6f2c899101f153ab714a5ab20bbf0e5e
Souheil <souheil.sultan@gmail.com>
2019-02-05 14:43:03 +0100

    ajout servlet

--- web-servlet/src/main/java/imie/HelloServlet.java
+++ web-servlet/src/main/java/imie/HelloServlet.java
@@ -1,8 +1,19 @@
 package imie;
 
-import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
 
-@WebServlet("hello")
-public class HelloServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Coucou");
+	}
 
 }
